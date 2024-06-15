0f8b6a06e92813f19a2914fdc2053efd8f342e6f
hyeonbungi <hyeonbungi@outlook.com>
2024-06-15 18:09:51 +0900

    반복자를 사용하도록 리팩터링

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,24 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("입력한 인수의 개수가 올바르지 않습니다.\n\
-            [사용법] unlucky-grep [search_query] [file_path]");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let search_query = args[1].clone();
-        let file_path = args[2].clone();
+        let search_query = match args.next() {
+            Some(arg) => arg,
+            None => {
+                return Err("검색 쿼리를 입력하지 않았습니다.\n\
+            [사용법] unlucky-grep [search_query] [file_path]")
+            }
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => {
+                return Err("파일 경로를 입력하지 않았습니다.\n\
+            [사용법] unlucky-grep [search_query] [file_path]")
+            }
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -50,28 +60,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 // 4. 아니라면 아무것도 안 한다.
 // 5. 매칭된 결과 리스트를 반환한다.
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
